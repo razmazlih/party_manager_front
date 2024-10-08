94dbe5a5cae0c6355bb590184436716346e8d87e
Raz <retraz123@gmail.com>
2024-10-08 17:26:07 +0300

    update url

--- src/services/api.js
+++ src/services/api.js
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
-const API_URL = 'http://127.0.0.1:8000/api/';
+// const API_URL = 'http://127.0.0.1:8000/api/';
 
-// const API_URL = 'https://party-manager-back.onrender.com/api/';
+const API_URL = 'https://party-manager-back.onrender.com/api/';
 
 
 const api = axios.create({
